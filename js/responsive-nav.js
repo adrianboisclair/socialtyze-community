4995d74fff31d73f70b34ab9afe1d1ba77a7665b
Adrian Boisclair <adrianboisclair@gmail.com>
2015-08-17 10:22:52 -0700

    added carousel, separated css js

---
+++ js/responsive-nav.js
@@ -0,0 +1,51 @@
+/**
+ * Created by adrian on 8/17/15.
+ */
+var header, logo, yPos, windowWidth, link;
+function yScroll() {
+    header = document.getElementById('header');
+    logo = document.getElementsByClassName("logo-sm-top");
+    link = document.getElementsByClassName("link");
+    yPos = window.pageYOffset;
+    windowWidth = window.innerWidth;
+    if(yPos > 50) {
+        header.style.height = "64px";
+        header.style.paddingTop = "10px";
+        header.style.paddingBottom = "30px";
+//            header.style.backgroundColor = "rgba(0,0,0,.8)";
+        header.style.backgroundColor = "transparent";
+        for( var i = 0; i < link.length; i++) {
+            link[i].style.paddingTop = "10px";
+        }
+        if(windowWidth <= 768) {
+            header.style.height = "75px";
+            header.style.paddingTop = "0px";
+            header.style.paddingBottom = "40px";
+            logo[0].style.width = "140px";
+            logo[0].style.marginTop = "10px";
+
+            for( var i = 0; i < link.length; i++) {
+                link[i].style.paddingTop = "0px";
+                link[i].style.fontSize = "12px";
+            }
+        }
+    } else {
+        header.style.height="95px";
+        header.style.paddingTop = "40px";
+        header.style.backgroundColor = "transparent";
+        for( var i = 0; i < link.length; i++) {
+            link[i].style.paddingTop = "5px";
+        }
+        if(windowWidth <= 768) {
+            logo[0].style.width = "180px";
+            header.style.height="95px";
+            header.style.paddingTop = "10px";
+            for( var i = 0; i < link.length; i++) {
+                link[i].style.fontSize = "14px";
+                link[i].style.paddingTop = "0px";
+            }
+
+        }
+    }
+}
+window.addEventListener("scroll", yScroll);
